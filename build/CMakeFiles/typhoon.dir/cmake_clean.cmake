aba3f6cba018058276563b6c7a46e65d43c30b89
janeloveless <jane_loveless@protonmail.com>
2022-06-14 23:41:40 +0000

    basic vector operations implemented

--- build/CMakeFiles/typhoon.dir/cmake_clean.cmake
+++ build/CMakeFiles/typhoon.dir/cmake_clean.cmake
@@ -1,5 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/typhoon.dir/src/main.cpp.o"
+  "CMakeFiles/typhoon.dir/src/main.cpp.o.d"
+  "typhoon"
+  "typhoon.pdb"
+)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang )
+foreach(lang CXX)
   include(CMakeFiles/typhoon.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
